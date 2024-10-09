778e2c3fbcf4eef3a42f819142ef96ab87e89b55
sunkenquest <sunkenquest@gmail.com>
2024-10-09 16:24:48 +0800

    chore: added entity, dto, repo, services

---
+++ src/main/java/com/CodeElevate/ExpenseTracker/sevices/expense/ExpenseServiceImpl.java
@@ -0,0 +1,9 @@
+package com.CodeElevate.ExpenseTracker.sevices.expense;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExpenseServiceImpl implements ExpenseService{
+}
