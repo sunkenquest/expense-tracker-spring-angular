efd3ea0a1c3b41e99277a974f6dc4c9edb224d73
m_.kco <83852126+sunkenquest@users.noreply.github.com>
2024-10-09 17:26:16 +0800

    feat: add update expense api (#6)

--- src/main/java/com/CodeElevate/ExpenseTracker/sevices/expense/ExpenseService.java
+++ src/main/java/com/CodeElevate/ExpenseTracker/sevices/expense/ExpenseService.java
@@ -10,4 +10,5 @@ public interface ExpenseService {
     Expense postExpense(ExpenseDTO expenseDTO);
     List<Expense> getAllExpenses();
     Expense getExpenseById(Long id);
+    Expense updateExpense(Long id, ExpenseDTO expenseDTO);
 }
