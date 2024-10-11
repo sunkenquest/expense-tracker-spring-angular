cea5c1525849b56d87ccc1bd33a52e51bf7f5991
m_.kco <83852126+sunkenquest@users.noreply.github.com>
2024-10-11 10:57:56 +0800

    feat: graph income and expense (#14)

--- src/main/java/com/CodeElevate/ExpenseTracker/repository/ExpenseRepository.java
+++ src/main/java/com/CodeElevate/ExpenseTracker/repository/ExpenseRepository.java
@@ -1,9 +1,14 @@
 package com.CodeElevate.ExpenseTracker.repository;
 
 import com.CodeElevate.ExpenseTracker.entity.Expense;
+import com.CodeElevate.ExpenseTracker.entity.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findByDateBetween(LocalDate startDate, LocalDate endDate);
 }
