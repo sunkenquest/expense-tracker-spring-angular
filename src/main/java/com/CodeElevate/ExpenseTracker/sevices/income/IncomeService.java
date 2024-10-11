b56dc5e4d8ed33d8460e408aa18125d46e09f2e1
sunkenquest <sunkenquest@gmail.com>
2024-10-11 09:45:21 +0800

    feat: get all income api

--- src/main/java/com/CodeElevate/ExpenseTracker/sevices/income/IncomeService.java
+++ src/main/java/com/CodeElevate/ExpenseTracker/sevices/income/IncomeService.java
@@ -3,6 +3,9 @@ package com.CodeElevate.ExpenseTracker.sevices.income;
 import com.CodeElevate.ExpenseTracker.dto.IncomeDTO;
 import com.CodeElevate.ExpenseTracker.entity.Income;
 
+import java.util.List;
+
 public interface IncomeService {
     Income postIncome(IncomeDTO incomeDTO);
+    List<IncomeDTO> getAllIncomes();
 }
--- src/main/java/com/CodeElevate/ExpenseTracker/sevices/income/IncomeServiceImp.java
+++ src/main/java/com/CodeElevate/ExpenseTracker/sevices/income/IncomeServiceImp.java
@@ -6,6 +6,10 @@ import com.CodeElevate.ExpenseTracker.repository.IncomeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class IncomeServiceImp implements IncomeService {
@@ -24,5 +28,12 @@ public class IncomeServiceImp implements IncomeService {
         income.setDescription(incomeDTO.getDescription());
 
         return incomeRepository.save(income);
+    }
+
+    public List<IncomeDTO> getAllIncomes() {
+        return incomeRepository.findAll().stream()
+                .sorted(Comparator.comparing(Income::getDate).reversed())
+                .map(Income::getIncomeDto)
+                .collect(Collectors.toList());
     }
 }
