647d63d51ea8461080d117c3f087ede492f7b991
sunkenquest <sunkenquest@gmail.com>
2024-10-11 10:56:50 +0800

    feat: graph income and expense

--- src/main/java/com/CodeElevate/ExpenseTracker/repository/IncomeRepository.java
+++ src/main/java/com/CodeElevate/ExpenseTracker/repository/IncomeRepository.java
@@ -4,6 +4,10 @@ import com.CodeElevate.ExpenseTracker.entity.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface IncomeRepository extends JpaRepository<Income, Long> {
+    List<Income> findByDateBetween(LocalDate startDate, LocalDate endDate);
 }
