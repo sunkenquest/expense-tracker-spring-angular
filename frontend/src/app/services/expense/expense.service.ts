da461370b4274eb53d5ff5b8f525223aab77644e
m_.kco <83852126+sunkenquest@users.noreply.github.com>
2024-10-11 16:49:01 +0800

    feat: call delete api from ui (#23)

--- frontend/src/app/services/expense/expense.service.ts
+++ frontend/src/app/services/expense/expense.service.ts
@@ -18,4 +18,8 @@ export class ExpenseService {
   getAllExpenses(): Observable<any> {
     return this.http.get(BASIC_URL + "api/expense/all")
   }
+
+  deleteExpense(id: number): Observable<any> {
+    return this.http.delete(BASIC_URL + `api/expense/${id}`)
+  }
 }
