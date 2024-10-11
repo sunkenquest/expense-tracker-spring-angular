62ca7080d4840c87e6ed6bfaf27f69b71dc0a539
sunkenquest <sunkenquest@gmail.com>
2024-10-11 11:44:06 +0800

    feat: balance, min, max income and expense

--- src/main/java/com/CodeElevate/ExpenseTracker/dto/StatsDTO.java
+++ src/main/java/com/CodeElevate/ExpenseTracker/dto/StatsDTO.java
@@ -7,7 +7,20 @@ import lombok.Data;
 @Data
 public class StatsDTO {
     private Double income;
+
     private Double expense;
+
     private Income latestIncome;
+
     private Expense latestExpense;
+
+    private Double balance;
+
+    private Double minIncome;
+
+    private Double maxIncome;
+
+    private Double minExpense;
+
+    private Double maxExpense;
 }
