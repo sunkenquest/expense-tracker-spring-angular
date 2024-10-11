607904bae889b59f3665b178ab688f20cb7fcfa3
m_.kco <83852126+sunkenquest@users.noreply.github.com>
2024-10-11 14:13:31 +0800

    feat: add post expense form component (#20)

---
+++ frontend/src/app/components/expense/expense.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-expense',
+  templateUrl: './expense.component.html',
+  styleUrls: ['./expense.component.scss']
+})
+export class ExpenseComponent {
+  expenseForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.expenseForm = this.fb.group({
+      title: [null, Validators.required],
+      amount: [null, Validators.required],
+      date: [null, Validators.required],
+      category: [null, Validators.required],
+      description: [null, Validators.required],
+    })
+  }
+}
