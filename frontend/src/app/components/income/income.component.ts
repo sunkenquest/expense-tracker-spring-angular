5a30d9dd98f09f4b4a6cf71fe79b862f5ad1a428
sunkenquest <sunkenquest@gmail.com>
2024-10-14 10:42:33 +0800

    feat: call post income from ui

---
+++ frontend/src/app/components/income/income.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { IncomeService } from 'src/app/services/income/income.service';
+
+@Component({
+  selector: 'app-income',
+  templateUrl: './income.component.html',
+  styleUrls: ['./income.component.scss']
+})
+export class IncomeComponent {
+
+  incomeForm!: FormGroup;
+  listOfCategory: any[] = ["Salary", "Freelancing", "Investments", "Stocks", "Bitcoin", "Bank Transfer", "YouTube", "Other"];
+
+  constructor(
+    private fb: FormBuilder,
+    private message: NzMessageService,
+    private incomeService: IncomeService,
+    private router: Router) { }
+
+
+  ngOnInit() {
+    this.incomeForm = this.fb.group({
+      title: [null, Validators.required],
+      amount: [null, Validators.required],
+      date: [null, Validators.required],
+      category: [null, Validators.required],
+      description: [null, Validators.required],
+    })
+  }
+
+  submitForm() {
+    this.incomeService.postIncome(this.incomeForm.value).subscribe(res => {
+      this.message.success("Income posted successfully", { nzDuration: 5000 })
+
+    }, error => {
+      this.message.error("Error while posting expense", { nzDuration: 5000 })
+    })
+  }
+}
