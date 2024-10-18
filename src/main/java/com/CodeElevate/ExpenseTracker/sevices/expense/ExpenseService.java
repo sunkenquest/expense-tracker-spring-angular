f6733ba033c4ef8ad718ba8ac5e2563d9cec974b
sunkenquest <sunkenquest@gmail.com>
2024-10-18 17:16:58 +0800

    chore: add user seeder, pagination fixes and button ajustments

--- src/main/java/com/CodeElevate/ExpenseTracker/sevices/expense/ExpenseService.java
+++ src/main/java/com/CodeElevate/ExpenseTracker/sevices/expense/ExpenseService.java
@@ -3,8 +3,6 @@ package com.CodeElevate.ExpenseTracker.sevices.expense;
 import com.CodeElevate.ExpenseTracker.dto.ExpenseDTO;
 import com.CodeElevate.ExpenseTracker.entity.Expense;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 
 public interface ExpenseService {
