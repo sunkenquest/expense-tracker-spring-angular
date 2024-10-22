0b4b779503f9cc9b55ad8da404ecc9dc4d3c601e
m_.kco <83852126+sunkenquest@users.noreply.github.com>
2024-10-22 10:57:35 +0800

    chore: add env port (#41)

--- frontend/src/app/services/expense/expense.service.ts
+++ frontend/src/app/services/expense/expense.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const BASIC_URL = "http://localhost:8080/";
+const BASIC_URL = process.env['PORT'] || "http://localhost:8080/";
 
 @Injectable({
   providedIn: 'root'
